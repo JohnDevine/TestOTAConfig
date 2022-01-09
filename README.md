4645d7d2aa405e87ffdc5fe25956d324932adfcc
Solar Blue <john.h.devine@gmail.com>
2022-01-09 20:52:15 +0700

    Add debug option

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ ___
 1. Look for a wifi network and connect to it. 
 __\(NOTE .. The first time   it starts up it will go straight to the webserver\)__
     * If it cannot find the one saved previously then open a captive portal web server and allow the user to connect to that and input the SSID and Password for the WiFi that you want to connect to and then to keep that entry and use that for subsequent boots.
+    * Turn on the onboard LED when in config mode.
         * __NOTE__ the credentials are held in NVR and the only way to totally remove them is to manually clear the NVR on the ESP.
           
           1. In VSC + Platformio extension\)Start CLI ... To get to CLI in LHS menu select PIO \(Ant Icon\)
@@ -26,9 +27,20 @@ __\(NOTE .. The first time   it starts up it will go straight to the webserver\)
              * pio run --target erase
           * __it may take more than one attempt__
 ![See Here](images/img01.png)
-1. Allow for OTA (Over The Air) updates by having a webserver on url:
+1. Allow depressing boot button on ESP32 DOit V1 board twice \(__NOTE:__ allow about 3 seconds between depresses\) will allow reconfiguration by webserver \(See above.\)
+
+2. Allow for OTA (Over The Air) updates by having a webserver on url:
    * http:\/\/(YourESP32IP)\/update 
      eg 
      http://192.168.2.42/update
      
      that will load a .bin up to the ESP32 and reboot.
+![See Here](images/img02.png)
+
+1. Add Debugging ability
+   * Use ArduinoTrace by Benoit Blanchon
+     * Extend it by using #define ARDUINOTRACE_ENABLE true \/\/ Enable ArduinoTrace \(false = disable, true = enable\) to enable\/disable serial.println\(\) statements.
+     * TRACE();
+     * DUMP(value);
+     * BREAK();
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,109 @@
-
+/****************************************************************************************************************************
+  Async_ConfigOnDoubleReset_minimal.ino
+  For ESP8266 / ESP32 boards
+  Built by Khoi Hoang https://github.com/khoih-prog/ESPAsync_WiFiManager
+  Licensed under MIT license
+ *****************************************************************************************************************************/
 #if !(defined(ESP32))
+#error This code is intended to run on the ESP32 platform! Please check your Tools->Board setting.
+#endif
+
+#define ARDUINOTRACE_ENABLE true // Tracing (false = disable, true = enable)
+#include <ArduinoTrace.h>        //https://github.com/bblanchon/ArduinoTrace
+
+#if defined(ESP32)
+#define USE_SPIFFS true
+#define ESP_DRD_USE_EEPROM true
+#else
 #error This code is intended to run on the ESP32 platform! Please check your Tools->Board setting.
 #endif
 #include <ESPAsync_WiFiManager.h>      //https://github.com/khoih-prog/ESPAsync_WiFiManager
 #include <ESPAsync_WiFiManager-Impl.h> //https://github.com/khoih-prog/ESPAsync_WiFiManager
+#define DRD_TIMEOUT 10
+#define DRD_ADDRESS 0
+#include <ESP_DoubleResetDetector.h> //https://github.com/khoih-prog/ESP_DoubleResetDetector
+DoubleResetDetector *drd;
+const int PIN_LED = 2;
+bool initialConfig = false;
 AsyncWebServer webServer(80);
+
+#include <AsyncElegantOTA.h>
 
 DNSServer dnsServer;
 
 void setup()
 {
-  // put your setup code here, to run once:
+
+#if (ARDUINOTRACE_ENABLE)
   Serial.begin(115200);
   while (!Serial)
     ;
   delay(200);
-  Serial.print("\nStarting Async_AutoConnect_ESP32_minimal on " + String(ARDUINO_BOARD));
+
+  Serial.print("\nStarting on " + String(ARDUINO_BOARD));
   Serial.println(ESP_ASYNC_WIFIMANAGER_VERSION);
+#endif
+  TRACE();
 
-  ESPAsync_WiFiManager ESPAsync_wifiManager(&webServer, &dnsServer, "Async_AutoConnect");
+  pinMode(PIN_LED, OUTPUT);
 
-  // ESPAsync_wifiManager.resetSettings();   //reset saved settings
-  ESPAsync_wifiManager.setAPStaticIPConfig(IPAddress(192, 168, 132, 1), IPAddress(192, 168, 132, 1), IPAddress(255, 255, 255, 0));
-  ESPAsync_wifiManager.autoConnect("AutoConnectAP");
-  if (WiFi.status() == WL_CONNECTED)
+  drd = new DoubleResetDetector(DRD_TIMEOUT, DRD_ADDRESS);
+  if (drd->detectDoubleReset())
   {
-    Serial.print(F("Connected. Local IP: "));
-    Serial.println(WiFi.localIP());
+    Serial.println(F("DRD"));
+    initialConfig = true;
+  }
+
+  ESPAsync_WiFiManager ESPAsync_wifiManager(&webServer, &dnsServer, "Async_ConfigOnDRD");
+
+  if (ESPAsync_wifiManager.WiFi_SSID() == "")
+  {
+    Serial.println(F("No AP credentials"));
+    initialConfig = true;
+  }
+  if (initialConfig)
+  {
+    Serial.println(F("Starting Config Portal"));
+    digitalWrite(PIN_LED, HIGH);
+    if (!ESPAsync_wifiManager.startConfigPortal())
+    {
+      Serial.println(F("Not connected to WiFi"));
+    }
+    else
+    {
+      Serial.println(F("connected"));
+    }
   }
   else
   {
-    Serial.println(ESPAsync_wifiManager.getStatus(WiFi.status()));
+    WiFi.mode(WIFI_STA);
+    WiFi.begin();
   }
-}
+  digitalWrite(PIN_LED, LOW);
+  unsigned long startedAt = millis();
+  Serial.print(F("After waiting "));
+  int connRes = WiFi.waitForConnectResult();
+  float waited = (millis() - startedAt);
+  Serial.print(waited / 1000);
+  Serial.print(F(" secs , Connection result is "));
+  Serial.println(connRes);
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println(F("Failed to connect"));
+  }
+  else
+  {
+    Serial.print(F("Local IP: "));
+    Serial.println(WiFi.localIP());
+  }
 
-void loop() {}
+  // Set up the OTA handler
+  webServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+               { request->send(200, "text/plain", "Hi! I am ESP32."); });
+  AsyncElegantOTA.begin(&webServer); // Start ElegantOTA
+  webServer.begin();
+}
+void loop()
+{
+  drd->loop();
+}
